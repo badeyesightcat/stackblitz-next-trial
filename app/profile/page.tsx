19b705468b229d93b72127cad7e37de6e7ee22bf
badeyesightcat <whitebigbabybird@gmail.com>
2024-06-10 13:32:34 +0900

    Initial commit

---
+++ app/profile/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import type { PageProps } from '@/types/common';
+
+const ProfilePage = (props: PageProps) => {
+  console.log(props);
+  return (
+    <>
+      <div>profile page </div>
+    </>
+  );
+};
+
+export default ProfilePage;
