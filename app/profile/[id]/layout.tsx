500ff4a83988a8bc93b226bf6f3fe4963205f6b6
badeyesightcat <whitebigbabybird@gmail.com>
2024-06-11 15:02:58 +0900

    remove unused layouts at the moment

--- app/profile/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { LayoutProps } from "@/types/common";
-import React from "react";
-
-const ProfileItemLayout = (props: LayoutProps) => {
-  // console.log(props);
-  return <div>this is ProfileItemLayout</div>;
-};
-
-export default ProfileItemLayout;
