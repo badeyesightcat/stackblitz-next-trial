f5c4e9f93ded99c5d0ce416026c6dd408b6191fd
badeyesightcat <whitebigbabybird@gmail.com>
2024-06-11 14:24:12 +0900

    add first route file

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,1 +1,12 @@
-import { NextResponse, NextRequest }
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    return NextResponse.json(
+      { message: "successfully conntected" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
