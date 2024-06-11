03305e8f9494effa9aa2aff11ed83580bf41e9ef
badeyesightcat <whitebigbabybird@gmail.com>
2024-06-11 14:25:13 +0900

    add styles onto menus for hover state

--- components/menus.tsx
+++ components/menus.tsx
@@ -5,10 +5,20 @@ const Menus = () => {
   return (
     <ul className="flex flex-1 gap-4">
       <li>
-        <Link href="/issue-board">Issue board</Link>
+        <Link
+          href="/issue-board"
+          className="px-2 py-1 leading-none hover:border-dashed border-transparent border-4 hover:border-[--primary]"
+        >
+          Issue board
+        </Link>
       </li>
       <li>
-        <Link href="/profile">profile</Link>
+        <Link
+          href="/profile"
+          className="px-2 py-1 leading-none hover:border-dashed border-transparent border-4 hover:border-[--primary]"
+        >
+          profile
+        </Link>
       </li>
     </ul>
   );
