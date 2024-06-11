d83a68bb8156f0b388feeadf80da25caf3b45ed4
badeyesightcat <whitebigbabybird@gmail.com>
2024-06-11 14:02:21 +0900

    add contents for not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <section className="flex flex-col items-center">
+      <h1>404</h1>
+      <p>
+        There is no page which you wanted to visit, please go back to Home and
+        try something else.
+      </p>
+      <Link href={"/"}>Go home</Link>
+    </section>
+  );
 };
 
 export default NotFoundPage;
