1d2f9b81266ef5d85da945fca854a9b42cbfa3b8
Beom Mee <whitebigbabybird@gmail.com>
2024-06-14 14:28:25 +0900

    modify navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Logo from "./logo";
-import LogoutButton from "./logoutButton";
 import Menus from "./menus";
 
 const Navbar = () => {
   return (
-    <nav className="flex gap-8 items-center">
+    <nav className="flex gap-8 items-center justify-between">
       <Logo />
       <Menus />
-      <LogoutButton />
     </nav>
   );
 };
