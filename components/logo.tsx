437f22817e046c01197f5d6354993c029b78458c
Beom Mee <whitebigbabybird@gmail.com>
2024-06-15 20:27:13 +0900

    Add styles and props for logo that can be added for footer purpose

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,13 +1,19 @@
-import React from "react";
 import fonts from "@/lib/fonts";
 import classNames from "classnames";
 
-const Logo = () => {
+interface LogoProps {
+  footer?: boolean;
+}
+
+const Logo = ({ footer }: LogoProps) => {
   return (
     <h1
       className={classNames(
         fonts.gwendolyn.className,
-        "font-bold text-7xl bg-[--light] text-[--dark] px-4 pb-8 -mt-2 leading-none relative before:w-full before:h-1 before:bg-white before:left-0 before:content-[''] before:block before:top-3 before:absolute before:z-10"
+        "font-bold bg-[--light] text-[--dark] leading-none relative before:w-full before:bg-white before:left-0 before:content-[''] before:block before:top-3 before:absolute before:z-10",
+        footer
+          ? "w-fit px-2 pb-4 text-4xl before:h-[.125rem] grayscale"
+          : "text-7xl px-4 pb-8 -mt-2 before:h-1"
       )}
     >
       issue tracker
