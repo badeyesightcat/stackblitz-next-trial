a3da15d7bd6f8f981c2c8d7dd6c5d1de838f0044
Beom Mee <whitebigbabybird@gmail.com>
2024-06-15 20:25:31 +0900

    Add footer contents

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Logo from "./logo";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="container mx-auto flex justify-between items-center">
+      <Logo footer={true} />
+      <p className="flex gap-8">
+        <Link
+          className="underline"
+          href={"https://github.com/badeyesightcat"}
+        >
+          Github
+        </Link>
+        <strong>&copy; 2024 Beom Mee</strong>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
