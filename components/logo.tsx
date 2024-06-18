01de680403eff732433f1b4cc5a1d73f4702fbee
Beom Mee <whitebigbabybird@gmail.com>
2024-06-18 17:48:04 +0900

    modify logo font and its style

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,14 +9,15 @@ const Logo = ({ footer }: LogoProps) => {
   return (
     <h1
       className={classNames(
-        fonts.gwendolyn.className,
-        "font-bold bg-[--light] text-[--dark] leading-none relative before:w-full before:bg-white before:left-0 before:content-[''] before:block before:top-3 before:absolute before:z-10",
+        fonts.comfortaa.className,
+        "font-bold bg-[--light] text-[--dark] leading-none relative before:w-full before:bg-white before:left-0 before:content-[''] before:absolute before:z-10",
         footer
-          ? "w-fit px-2 pb-4 text-4xl before:h-[.125rem] grayscale"
-          : "text-7xl px-4 pb-8 -mt-2 before:h-1"
+          ? "w-fit px-2 pb-4 text-2xl before:h-[.125rem] grayscale before:top-1"
+          : "text-4xl px-4 pb-8 -mt-[.25rem] before:h-[.125rem] before:top-2"
       )}
     >
-      Charlie&apos;s Desk
+      Charlie&apos;s <br />
+      Desk
     </h1>
   );
 };
