a8bdbe6d49172f672b28e28b003eb8215ffa16d4
Beom Mee <whitebigbabybird@gmail.com>
2024-06-22 20:15:54 +0900

    Generate IssueTableBodyRow component

---
+++ components/IssueTableBodyRow.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import IssueTableCell from "./IssueTableCell";
+
+interface IssueTableBodyRowCellProps {
+  title: string;
+  collectionType: string;
+  tag: Array<string>;
+  owner: string;
+  date: string | Date;
+  priority: string;
+}
+
+interface IssueTableBodyRowProps {
+  row: IssueTableBodyRowCellProps;
+}
+
+const IssueTableBodyRow = ({ row }: IssueTableBodyRowProps) => {
+  const rowEntries = Object.entries(row);
+
+  return (
+    <div className="flex">
+      {rowEntries?.map((entry, idx) => (
+        <IssueTableCell
+          key={`${entry[0]}-${entry[1]}`}
+          cellType="td"
+          isLonger={idx === 0}
+        >
+          {entry[1]}
+        </IssueTableCell>
+      ))}
+    </div>
+  );
+};
+
+export default IssueTableBodyRow;
