fe82777de5bd4ca394fbc8d4a0c845e12ed703ec
Beom Mee <whitebigbabybird@gmail.com>
2024-06-22 20:12:10 +0900

    Get issue data in the IssueTableBody component itself not the parent of it

--- components/IssueTableBody.tsx
+++ components/IssueTableBody.tsx
@@ -1,18 +1,61 @@
 "use client";
 import React from "react";
+import IssueTableBodyRow from "./IssueTableBodyRow";
 
-interface IssueTableBodyProps {
-  title: string;
-  collectionType: string;
-  tag: Array<string>;
-  owner: string;
-  date: Date;
-  priority: string;
-}
+const issueTableBodyRows = [
+  {
+    title: "Plan the first sprint",
+    collectionType: "Backlog",
+    tag: [],
+    owner: "Charlie",
+    date: "",
+    priority: "High priority",
+  },
+  {
+    title: "Fix duplicate notifications",
+    collectionType: "Backlog",
+    tag: ["Bug"],
+    owner: "",
+    date: "",
+    priority: "",
+  },
+  {
+    title: "Enable checkout on mobile",
+    collectionType: "Backlog",
+    tag: ["Mobile"],
+    owner: "",
+    date: "",
+    priority: "",
+  },
+  {
+    title: "Add intercom integration",
+    collectionType: "Backlog",
+    tag: ["Integration"],
+    owner: "",
+    date: "",
+    priority: "",
+  },
+  {
+    title: "Sprint item template",
+    collectionType: "Backlog",
+    tag: [],
+    owner: "",
+    date: "",
+    priority: "",
+  },
+];
 
-const IssueTableBody = (props: IssueTableBodyProps) => {
-  console.log(props);
-  return <div>IssueTableBody</div>;
+const IssueTableBody = () => {
+  return (
+    <div>
+      {issueTableBodyRows?.map((row) => (
+        <IssueTableBodyRow
+          key={`${row.title}-${row.date}`}
+          row={row}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default IssueTableBody;
