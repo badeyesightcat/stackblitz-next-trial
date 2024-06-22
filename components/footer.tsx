2eb5f7e70e2624fd92f7ceee877bb40ccb86992c
Beom Mee <whitebigbabybird@gmail.com>
2024-06-22 17:27:53 +0900

    Add styles onto the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex justify-between items-center">
-      <Logo footer={true} />
-      <p className="flex gap-8">
-        <Link
-          className="underline"
-          href={"https://github.com/badeyesightcat"}
-        >
-          Github
-        </Link>
-        <strong>&copy; 2024 Beom Mee</strong>
-      </p>
+    <footer className="border-y-2 border-y-[--light] py-8">
+      <div className="container mx-auto flex justify-between items-center">
+        <Logo footer={true} />
+        <p className="flex gap-8">
+          <Link
+            className="underline"
+            href={"https://github.com/badeyesightcat"}
+          >
+            Github
+          </Link>
+          <strong>&copy; 2024 Beom Mee</strong>
+        </p>
+      </div>
     </footer>
   );
 };
