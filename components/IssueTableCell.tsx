97c6300d0fbdb3e97a5b00ebfb0acadc3143e456
Beom Mee <whitebigbabybird@gmail.com>
2024-06-22 20:16:45 +0900

    Modify cell styles according to its type and the order in the row

--- components/IssueTableCell.tsx
+++ components/IssueTableCell.tsx
@@ -4,19 +4,25 @@ interface IssueCellProps {
   cellType: string;
   // value: string | number | object | undefined;
   children: any;
-  isLastHead: boolean;
+  isLastHead?: boolean;
+  isLonger?: boolean;
 }
 
-const IssueTableCell = ({ children, cellType, isLastHead }: IssueCellProps) => {
+const IssueTableCell = ({
+  children,
+  cellType,
+  isLastHead,
+  isLonger,
+}: IssueCellProps) => {
   return (
     <div
-      className={`relative py-2 flex-auto capitalize text-md text-center ${
+      className={`relative flex items-center gap-1 py-2 px-4 flex-auto capitalize text-sm ${
         cellType === "th" ? "font-bold" : ""
       } ${
-        !isLastHead
+        isLastHead !== undefined && !isLastHead
           ? "after:absolute after:content-[''] after:w-[1px] after:bottom-0 after:bg-gray-300 after:top-0 after:left-full"
           : ""
-      }`}
+      } ${isLonger ? "basis-80" : "basis-40"}`}
     >
       {children}
     </div>
