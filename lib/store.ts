e8687152a29a726e18d470fb39d48f8aaa97d9f3
Beom Mee <whitebigbabybird@gmail.com>
2024-07-24 17:53:47 +0900

    feat: generate store definition

---
+++ lib/store.ts
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import issuesReducer from "@/lib/features/issues/issuesSlice";
+import uiReducer from "@/lib/features/ui/uiSlice";
+
+const rootReducer = combineReducers({
+  issues: issuesReducer,
+  ui: uiReducer,
+});
+
+export const makeStore = () => {
+  console.log(rootReducer);
+  return configureStore({
+    reducer: rootReducer,
+    // middleware: null,
+  });
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
