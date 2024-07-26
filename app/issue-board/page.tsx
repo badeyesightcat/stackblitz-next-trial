8ae5000e77138b08391a0e8506e67f2354f4eaed
Beom Mee <whitebigbabybird@gmail.com>
2024-07-26 16:38:59 +0900

    feat: Apply store value for rendering detail section

--- app/issue-board/page.tsx
+++ app/issue-board/page.tsx
@@ -1,13 +1,18 @@
-import IssueTable from "@/components/IssueTable";
-import IssueTrackingHeader from "@/components/IssueTrackingHeader";
+"use client";
+import IssueContentSection from "@/components/IssueContentSection";
+import IssueListSection from "@/components/IssueListSection";
 import React from "react";
+import { useAppSelector } from "@/lib/hooks";
+import { selectDetailViewVisibility } from "@/lib/features/ui/uiSlice";
 
 const IssueBoardPage = () => {
+  const detailViewVisibility = useAppSelector(selectDetailViewVisibility);
+
   return (
-    <>
-      <IssueTrackingHeader />
-      <IssueTable />
-    </>
+    <div className="flex px-5 bg-slate-100">
+      <IssueListSection />
+      {detailViewVisibility !== 0 && <IssueContentSection />}
+    </div>
   );
 };
 
