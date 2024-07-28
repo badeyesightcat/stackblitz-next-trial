06b54d4b0dca7e09ecd806f9c3a5827f443df3dc
Beom Mee <whitebigbabybird@gmail.com>
2024-07-28 15:38:15 +0900

    replace issues data from a local component into store and use it instead

--- components/IssueTableBody.tsx
+++ components/IssueTableBody.tsx
@@ -1,59 +1,15 @@
 "use client";
 import React from "react";
 import IssueTableBodyRow from "./IssueTableBodyRow";
-
-const issueTableBodyRows = [
-  {
-    title: "Plan the first sprint",
-    collectionType: "Backlog",
-    tag: ["Integration"],
-    reportedBy: "Charlie",
-    assignedTo: "Daria",
-    priority: "High priority",
-    date: new Date().toLocaleDateString(),
-  },
-  {
-    title: "Fix duplicate notifications",
-    collectionType: "Backlog",
-    tag: ["Bug"],
-    reportedBy: "Charlie",
-    assignedTo: "Lizzy",
-    priority: "Low priority",
-    date: new Date().toLocaleDateString(),
-  },
-  {
-    title: "Enable checkout on mobile",
-    collectionType: "Backlog",
-    tag: ["Mobile"],
-    reportedBy: "Charlie",
-    assignedTo: "Dolores",
-    priority: "High priority",
-    date: new Date().toLocaleDateString(),
-  },
-  {
-    title: "Add intercom integration",
-    collectionType: "Backlog",
-    tag: ["Integration"],
-    reportedBy: "Charlie",
-    assignedTo: "Nick",
-    priority: "Low priority",
-    date: new Date().toLocaleDateString(),
-  },
-  {
-    title: "Sprint item template",
-    collectionType: "Backlog",
-    tag: ["Styling"],
-    reportedBy: "Charlie",
-    assignedTo: "Bazos",
-    priority: "Medium priority",
-    date: new Date().toLocaleDateString(),
-  },
-];
+import { useAppSelector } from "@/lib/hooks";
+import { selectIssues } from "@/lib/features/issues/issuesSlice";
 
 const IssueTableBody = () => {
+  const issues = useAppSelector(selectIssues);
+
   return (
     <div className="items-stretch grow">
-      {issueTableBodyRows?.map((row) => (
+      {issues?.map((row) => (
         <IssueTableBodyRow
           key={`${row.title}-${row.date}`}
           row={row}
