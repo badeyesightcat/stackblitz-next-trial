69726638b9a567e11fde4a7b2dab9ed74b3dd67f
Beom Mee <whitebigbabybird@gmail.com>
2024-07-28 15:13:28 +0900

    feat: add issue prop and render issue details onto the right-hand side

--- components/IssueContent.tsx
+++ components/IssueContent.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const IssueContent = () => {
-  return <div className="flex-1">IssueContent</div>;
+const IssueContent = (props: any) => {
+  const { issue } = props;
+  return (
+    <div className="flex-1">
+      {!issue && (
+        <div className="flex items-center justify-center h-full flex-col">
+          <strong className="opacity-40 text-lg">Nothing selected</strong>
+          <p className="opacity-50">
+            Select an item or collection on the left.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default IssueContent;
