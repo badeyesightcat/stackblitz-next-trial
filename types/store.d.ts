a8ac5dab6f6d52d1453e804cabfbb248d9a4aa89
Beom Mee <whitebigbabybird@gmail.com>
2024-08-03 17:01:54 +0900

    replace type definitions into the related slice file

--- types/store.d.ts
+++ types/store.d.ts
@@ -1,15 +1,1 @@
 export type IssuesAdd = "issues/add";
-
-export interface IssuesState {
-  list: Issue[];
-}
-
-export interface Issue {
-  title: string;
-  collectionType: string;
-  tag: string[];
-  reportedBy: string;
-  assignedTo: string;
-  priority: string;
-  date: string;
-}
